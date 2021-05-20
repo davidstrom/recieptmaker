ed1643599e9056d72482706135cb364871bb5022
David Ström <david.strom@callistaenterprise.se>
2021-05-20 14:59:00 +0200

    Changed raw bool to constant x

--- ReceptMaker/AppDelegate.swift
+++ ReceptMaker/AppDelegate.swift
@@ -14,8 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let x: Bool = true
         // Override point for customization after application launch.
-        return true
+        return x
     }
 
     // MARK: UISceneSession Lifecycle
