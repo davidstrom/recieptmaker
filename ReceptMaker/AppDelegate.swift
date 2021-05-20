035c28da0a415f737be9813f23754c31e3c939bf
David Ström <david.strom@callistaenterprise.se>
2021-05-20 15:04:40 +0200

    Removed comment

--- ReceptMaker/AppDelegate.swift
+++ ReceptMaker/AppDelegate.swift
@@ -15,7 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let x: Bool = true
-        // Override point for customization after application launch.
         return x
     }
 
